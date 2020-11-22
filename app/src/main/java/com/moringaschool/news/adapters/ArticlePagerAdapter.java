416087948caf77ffe58e1a28cce40e68e21bb9fc
Ashisoma <okalionesmus@gmail.com>
2020-11-23 01:26:28 +0300

    add fragments and editing the layouts

---
+++ app/src/main/java/com/moringaschool/news/adapters/ArticlePagerAdapter.java
@@ -0,0 +1,36 @@
+package com.moringaschool.news.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.moringaschool.news.models.Article;
+
+import java.util.List;
+
+public class  ArticlePagerAdapter extends FragmentPagerAdapter {
+
+    private List<Article> mArticles;
+
+    public ArticlePagerAdapter(FragmentManager fm, int behavior, List<Article> mArticles) {
+        super(fm, behavior);
+        this.mArticles = mArticles;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return mArticles.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position){
+        return mArticles.get(position).getTitle();
+    }
+}
--- app/src/main/java/com/moringaschool/news/ui/ArticleDetailActivity.java
+++ app/src/main/java/com/moringaschool/news/ui/ArticleDetailActivity.java
@@ -1,16 +1,41 @@
 package com.moringaschool.news.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.os.Parcel;
 
 import com.moringaschool.news.R;
+import com.moringaschool.news.adapters.ArticlePagerAdapter;
+import com.moringaschool.news.models.Article;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ArticleDetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.viewPager) ViewPager mViewPager;
+    private List<Article> mArticles;
+    private ArticlePagerAdapter adapterViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_detail);
+        ButterKnife.bind(this);
+
+
+        mArticles = Parcels.unwrap(getIntent().getParcelableExtra("articles"));
+        int startPosition = getIntent().getIntExtra("position",0);
+
+        adapterViewPager = new ArticlePagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, mArticles);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startPosition);
     }
 }
