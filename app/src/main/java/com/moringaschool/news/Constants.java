a70ae5fd8b05ee7c889ac9174ceb9068ecbafaf1
langat justus <justuslangat78@gmail.com>
2020-11-30 11:22:12 +0300

    change the layout

--- app/src/main/java/com/moringaschool/news/Constants.java
+++ app/src/main/java/com/moringaschool/news/Constants.java
@@ -2,5 +2,5 @@ package com.moringaschool.news;
 
 public class Constants {
     public static final String NEWS_BASE_URL = "https://newsapi.org/v2/";
-    public static final String NEWS_API_KEY = BuildConfig.NEWS_API_KEY;
+    public static final String NEWS_API_KEY = "bb5a1e8ca6334607b8aec9a1b689c2da";
 }
